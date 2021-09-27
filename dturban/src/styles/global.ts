013891d23c54b079cdd26af531c200eb812e901f
alucard <devjrteste@gmail.com>
2021-09-26 23:54:22 -0300

    chore: responsividade nas fonts

--- dturban/src/styles/global.ts
+++ dturban/src/styles/global.ts
@@ -11,6 +11,17 @@ export const GlobalStyle = createGlobalStyle`
     box-sizing: border-box;
 }
 
+//font-size: 16px Desktop
+html{
+    @media(max-width: 1080px) {
+        font-size: 93.75%; //15px
+    }
+
+    @media(max-width: 720px) {
+        font-size: 87.5%; //14px
+    }
+}
+
 body {
     background: var(--background);
     -webkit-font-smoothing: antialiased;
