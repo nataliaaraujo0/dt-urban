d98d36f55ceca504b51b2290a353221a0fd324b8
alucard <devjrteste@gmail.com>
2021-09-26 23:21:52 -0300

    test: testando css

--- dturban/src/assets/styles/global.ts
+++ dturban/src/assets/styles/global.ts
@@ -1,10 +1,18 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
+
+:root {
+    --background:#2C2C2C;
+}
 *{
     margin: 0;
     padding: 0;
     box-sizing: border-box;
 }
 
+body{
+    background: var(--background);
+    -webkit-font-smoothing:antialiased;
+}
 `;
