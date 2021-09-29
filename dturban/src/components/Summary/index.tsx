3b31dc99071ef93af4098b41f3be401363584ef5
alucard <devjrteste@gmail.com>
2021-09-29 17:52:31 -0300

    chore: header`s do component Summary finalizados

--- dturban/src/components/Summary/index.tsx
+++ dturban/src/components/Summary/index.tsx
@@ -1,4 +1,7 @@
-import EntradasImg from '../../assets/entradas.svg'
+import EntradasImg from '../../assets/entradas.svg';
+import SaidasImg from '../../assets/saidas.svg';
+import TotalImg from '../../assets/total.svg';
+
 import { Container } from "./style";
 
 export function Summary() {
@@ -9,6 +12,23 @@ export function Summary() {
                     <p>Entradas</p>
                     <img src={EntradasImg} alt="Entradas" />
                 </header>
+                <strong>R$100,00</strong>
+            </div>
+
+            <div>
+                <header>
+                    <p>Saídas</p>
+                    <img src={SaidasImg} alt="Saídas" />
+                </header>
+                <strong> - R$80,00</strong>
+            </div>
+
+            <div>
+                <header>
+                    <p>Saídas</p>
+                    <img src={TotalImg} alt="Total" />
+                </header>
+                <strong>R$20,00</strong>
             </div>
         </Container>
     );
