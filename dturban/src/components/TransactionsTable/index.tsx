37e1b187fb046d26db74da7c9cbb11977648ce7d
alucard <devjrteste@gmail.com>
2021-10-02 20:19:09 -0300

    chore: aplicndo miragejs no useEffect

--- dturban/src/components/TransactionsTable/index.tsx
+++ dturban/src/components/TransactionsTable/index.tsx
@@ -1,11 +1,11 @@
 import { useEffect } from "react";
+import { api } from "../../services/api";
 import { Container } from "../TransactionsTable/styles";
 
 export function TransactionsTable() {
 
     useEffect(() => {
-        fetch('http://localhost:3000/api/transactions')
-            .then(response => response.json())
+        api.get('/transactions')
             .then(data => console.log(data))
 
     }, [])
