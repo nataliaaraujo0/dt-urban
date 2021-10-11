58e9ed5a9ea3a7e418e2c6f93a90163f917010fb
alucard <devjrteste@gmail.com>
2021-10-10 22:55:16 -0300

    chore: criando estrutura html do form modal

--- dturban/src/components/NewTransactionModal/index.tsx
+++ dturban/src/components/NewTransactionModal/index.tsx
@@ -1,4 +1,5 @@
 import Modal from 'react-modal';
+import { Container } from './styles';
 
 interface NewTransactionModalProps {
     isOpen: boolean;
@@ -10,7 +11,28 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
         <Modal isOpen={isOpen}
             onRequestClose={onRequestClose}
         >
-            <h2>Cadastrar transação</h2>
+            <Container>
+                <h2>Cadastrar transação</h2>
+
+                <input
+                    placeholder="Título"
+                    type="text"
+                />
+
+                <input
+                    placeholder="Valor"
+                    type="number"
+                />
+
+                <input
+                    placeholder="Categoria"
+                    type="text"
+                />
+
+                <button type="submit">
+                    Cadastrar
+                </button>
+            </Container>
         </Modal>
     )
 }
