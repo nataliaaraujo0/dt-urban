9c2296f561ec0353e55410a39a2e87682dcc8e38
alucard <devjrteste@gmail.com>
2021-10-10 21:56:36 -0300

    refector!: repasse - react

--- dturban/src/App.tsx
+++ dturban/src/App.tsx
@@ -2,13 +2,32 @@ import styled from 'styled-components'
 import { Dashoboard } from './components/Dashboard';
 import { Header } from './components/Header';
 import { GlobalStyle } from "./styles/global";
-
+import Modal from 'react-modal';
+import { useState } from 'react';
 
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false);
+
+  }
+
   return (
     <>
       <Header />
       <Dashoboard />
+
+      <Modal isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      >
+        <h2>Cadastrar transação</h2>
+      </Modal>
+
       <GlobalStyle />
     </>
   );
--- dturban/src/components/Header/index.tsx
+++ dturban/src/components/Header/index.tsx
@@ -4,16 +4,7 @@ import { Container, Content } from './styles'
 import Modal from 'react-modal';
 
 export function Header() {
-    const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
-
-    function handleOpenNewTransactionModal() {
-        setIsNewTransactionModalOpen(true);
-    }
-
-    function handleCloseNewTransactionModal() {
-        setIsNewTransactionModalOpen(false);
-    }
-
+   
     return (
         <Container>
             <Content>
@@ -22,11 +13,6 @@ export function Header() {
                     Nova transação
                 </button>
 
-                <Modal isOpen={isNewTransactionModalOpen}
-                    onRequestClose={handleCloseNewTransactionModal}
-                >
-                    <h2>Cadastrar transação</h2>
-                </Modal>
             </Content>
         </Container>
     )
