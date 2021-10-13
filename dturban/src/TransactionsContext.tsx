a18715578bc0da9a9e5279cc3a0f7b87017bc34b
alucard <devjrteste@gmail.com>
2021-10-13 01:43:53 -0300

    chore: criando interface com referencia em uma a criada, mas omitindo dados

--- dturban/src/TransactionsContext.tsx
+++ dturban/src/TransactionsContext.tsx
@@ -1,7 +1,7 @@
 import { createContext, ReactNode, useEffect, useState } from "react";
 import { api } from "./services/api";
 
-interface Transactions {
+interface Transaction {
     id: number;
     title: string;
     amount: number;
@@ -14,15 +14,21 @@ interface TransactionsProviderProps {
     children: ReactNode;
 }
 
-export const TransactionsContext = createContext<Transactions[]>([]);
+type TransactionInput = Omit<Transaction, 'id' | 'createdAt'>
+
+export const TransactionsContext = createContext<Transaction[]>([]);
 
 export function TransactionsProvider({ children }: TransactionsProviderProps) {
-    const [transactions, setTransactions] = useState<Transactions[]>([]);
+    const [transactions, setTransactions] = useState<Transaction[]>([]);
 
     useEffect(() => {
         api.get('/transactions')
             .then(response => setTransactions(response.data.transactions))
     }, []);
+
+    function createTransaction(transaction: TransactionInput) {
+        api.post('/transactions', transaction)
+    }
 
     return (
         <TransactionsContext.Provider value={transactions}>
--- dturban/src/components/NewTransactionModal/index.tsx
+++ dturban/src/components/NewTransactionModal/index.tsx
@@ -1,34 +1,30 @@
 import Modal from 'react-modal';
+import { FormEvent, useState, useContext } from 'react';
 import { Container, RadioBox, TransactionTypeContainer } from './styles';
 import closeImg from '../../assets/close.svg';
 import EntradaImg from '../../assets/entradas.svg';
 import SaidaImg from '../../assets/saidas.svg';
-import { FormEvent, useState } from 'react';
 import { api } from '../../services/api';
+import { TransactionsContext } from '../../TransactionsContext';
 interface NewTransactionModalProps {
     isOpen: boolean;
     onRequestClose: () => void;
 }
 
 export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
+
+    const transctions = useContext(TransactionsContext);
+
     const [title, setTitle] = useState('');
     const [value, setValue] = useState(0);
     const [category, setCategory] = useState('');
-
 
     const [type, setType] = useState('deposit');
 
     function handleCreateNewTransaction(event: FormEvent) {
         event.preventDefault();
 
-        const data = ({
-            title,
-            value,
-            category,
-            type
-        });
-
-        api.post('/transactions', data)
+     
     }
 
     return (
